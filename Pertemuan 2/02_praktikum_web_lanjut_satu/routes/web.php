6865978af2e33aa6f99f5124fa1dba97216bbb04
BimaBayuUWUUU <bimabayus7994@gmail.com>
2023-02-22 22:02:29 +0700

    Praktikum 1

--- Pertemuan 2/02_praktikum_web_lanjut_satu/routes/web.php
+++ Pertemuan 2/02_praktikum_web_lanjut_satu/routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    echo "Selamat Datang";
+   });
+
+Route::get('/about', function () {
+ echo "2141720019 || Bima Bayu Saputra";
+    });
+
+Route::get('/articles/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID $id";
+   });
