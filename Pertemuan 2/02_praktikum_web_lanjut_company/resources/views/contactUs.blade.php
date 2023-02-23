d1293bb1a4bfc7294f5207e24f489676b28b0fd2
BimaBayuUWUUU <bimabayus7994@gmail.com>
2023-02-23 14:39:57 +0700

    Praktikum 3

---
+++ Pertemuan 2/02_praktikum_web_lanjut_company/resources/views/contactUs.blade.php
@@ -0,0 +1,12 @@
+@if(session('sukses'))
+<div>{{session('sukses')}}</div>
+@endif
+
+<form method="POST" action="{{route('contactUs.store')}}">
+    @csrf
+<h1>Kontak</h1>
+<input type="text" name="name" placeholder="Nama">
+<input type="text" name="email" placeholder="Email">
+<input type="text" name="password" placeholder="Password">
+<button type="submit" name="submit" value="submit">Kirim</button>
+</form>
--- Pertemuan 2/02_praktikum_web_lanjut_company/routes/web.php
+++ Pertemuan 2/02_praktikum_web_lanjut_company/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContactUsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,7 +13,50 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+/*No 1*/
 Route::get('/', function () {
-    return view('welcome');
+    return "Menampilkan halaman awal website";
+   });
+/*No 2*/
+Route::prefix('category')->group(function () {
+    Route::get('/marbel-edu-games', function () {
+        echo"Menampilkan daftar product Marbel Edu Games";
+    });
+    Route::get('/marbel-and-friends-kids-games', function () {
+        echo"Menampilkan daftar product Marbel and Friends Kids Games";
+    });
+    Route::get('/riri-story-books', function () {
+        echo"Menampilkan daftar product Riri Story Books";
+    });
+    Route::get('/kolak-kids-songs', function () {
+        echo"Menampilkan daftar product Kolak Kids Songs";
+    });
 });
+/*No 3*/
+Route::get('/news/{link?}', function ($link = null) {
+    return $link;
+});
+
+Route::get('/news/{link?}', function ($link = 'Not Found') {
+    return "Menampilkan Daftar berita (".$link.")";
+}); 
+/*No 4*/
+Route::prefix('program')->group(function () {
+    Route::get('/karir', function () {
+        echo"Menampilkan Daftar Program Karir";
+    });
+    Route::get('/magang', function () {
+        echo"Menampilkan Daftar Program Magang";
+    });
+    Route::get('/kunjungan-industri', function () {
+        echo"Menampilkan Daftar Program Kunjungan Industri";
+    });
+});
+/*No 5*/
+Route::get('/about-us', function () {
+    return "Bima Bayu Saputra || 2141720019";
+   });
+/*No 6*/
+Route::resource('contactUs', ContactUsController::class)->only([
+    'index', 'store'
+]);
